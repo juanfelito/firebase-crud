e433f41a2d15927fb3d3e05e21154bf3305ab5b1
Felipe <felipe.balcero45@com>
2018-11-27 13:42:17 -0500

    Real-time listener added

--- app.js
+++ app.js
@@ -30,10 +30,24 @@ function renderCafe(doc) {
 
 // Getting data
 
-db.collection('cafes').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-        renderCafe(doc);        
-    });
+// db.collection('cafes').where('city', '==', "Marioland").orderBy('name').get().then((snapshot) => {
+//     snapshot.docs.forEach(doc => {
+//         renderCafe(doc);        
+//     });
+// });
+
+// Real-time listener
+
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        if(change.type == 'added'){
+            renderCafe(change.doc);
+        } else if (change.type == 'removed') {
+            let li = cafeList.querySelector('[data-id="' + change.doc.id + '"]');
+            cafeList.removeChild(li);
+        }
+    })    
 });
 
 // Saving data
